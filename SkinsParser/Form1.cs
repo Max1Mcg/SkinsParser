49a292315f51cd5335adcc3f5d7219b71ce3ec74
Max1M <leggo2001@mail.ru>
2023-02-14 00:08:56 +0300

    Add button to CsMoney

--- SkinsParser/Form1.cs
+++ SkinsParser/Form1.cs
@@ -1,18 +1,38 @@
-﻿using SkinsParser.Core.csgotm;
+﻿using Newtonsoft.Json.Linq;
+using SkinsParser.Core.csgotm;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AngleSharp.Html.Parser;
+using System.Runtime.Remoting.Contexts;
+using AngleSharp.Html.Dom;
+using System.IO;
+using System.Net;
+using System.Data.SqlTypes;
+using System.Xml.Linq;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+using AngleSharp.Io;
+using SkinsParser.Core;
+using Json.Net;
+using System.Numerics;
+using SkinsParser.Core.csmoney;
+using System.Reactive;
 
 namespace SkinsParser
 {
     public partial class Form1 : Form
     {
+        CsgotmWorker csgotm;
+        CsMoneyWorker csMoney;
         public Form1()
         {
             InitializeComponent();
@@ -25,8 +45,19 @@ namespace SkinsParser
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var csgotm = new CsgotmWorker("", "");
-            csgotm.GetItem("AWP | Worm God (Factory New)");
+            csgotm = new CsgotmWorker("", "");
+            listBox1.Items.Add(csgotm.GetItem("AWP | Worm God (Factory New)"));
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            csMoney = new CsMoneyWorker("", "");
+            listBox1.Items.Add(await csMoney.GetItem("M4A4%20Neo-Noir"));
         }
     }
 }
