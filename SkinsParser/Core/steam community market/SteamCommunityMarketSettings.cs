dbe0661f1a7f11c77a983f4a69871bc9ccc00285
Max1M <leggo2001@mail.ru>
2023-02-19 23:05:32 +0300

    Add Class for new site and Class for settings.

---
+++ SkinsParser/Core/steam community market/SteamCommunityMarketSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace SkinsParser.Core
+{
+    internal class SteamCommunityMarketSettings : ISiteSettings, IItemSettings
+    {
+        public string Url { get; } = "https://steamcommunity.com/market/";
+        public string Name { get; set; }
+        public string Quality { get; set; }
+        public string Skin { get; set; }
+        public string priceViewUrl { get; } = "priceoverview/?appid=730&market_hash_name=";
+        public SteamCommunityMarketSettings() {
+            Name = default(string);
+            Quality = default(string);
+            Skin = default(string);
+        }
+        public SteamCommunityMarketSettings(string name, string quality, string skin)
+        {
+            Name = name;
+            Quality = quality;
+            Skin = skin;
+        }
+
+    }
+}
---
+++ SkinsParser/Core/steam community market/SteamCommunityMarketWorker.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using SkinsParser.Core.csgotm;
+using SkinsParser.Core.csmoney.cb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkinsParser.Core.steam_community_market
+{
+    internal class SteamCommunityMarketWorker
+    {
+        SteamCommunityMarketSettings steamCommunityMarketSettings = null;
+        public SteamCommunityMarketWorker(string weapon, string quality, string skin) {
+            steamCommunityMarketSettings = new SteamCommunityMarketSettings(weapon, quality, skin);
+        }
+        public async Task<string> GetItem()
+        {
+            var personalUrlForItem = $"{steamCommunityMarketSettings.Name}%20%7C%20{steamCommunityMarketSettings.Skin}%20%28{steamCommunityMarketSettings.Quality}%29&currency=1";
+            using (var client = new HttpClient())
+            {
+                var sb = new StringBuilder(steamCommunityMarketSettings.Url);
+                sb.Append(steamCommunityMarketSettings.priceViewUrl);
+                sb.Append(personalUrlForItem);
+                var content = await client.GetStringAsync(sb.ToString());
+                var json = JObject.Parse(content);
+                return Math.Round(Convert.ToDouble(json["lowest_price"].ToString().Substring(1).Replace('.', ',')) * (await DollarRate.GetDollarRate()), 2).ToString();
+            }
+        }
+        public string GetURL()
+        {
+            return steamCommunityMarketSettings.Url;
+        }
+    }
+}
