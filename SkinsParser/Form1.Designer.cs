7140f1b42a3ad3589be83a991e949095e0bacfde
Max1M <leggo2001@mail.ru>
2023-02-19 23:06:13 +0300

    Change some UI elements

--- SkinsParser/Form1.Designer.cs
+++ SkinsParser/Form1.Designer.cs
@@ -28,66 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Quality = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SkinName = new System.Windows.Forms.TextBox();
+            this.WeaponName = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(609, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выберите качество";
             // 
-            // button2
+            // Quality
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(609, 112);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Quality.FormattingEnabled = true;
+            this.Quality.Items.AddRange(new object[] {
+            "Factory New",
+            "Minimal Wear",
+            "Field-Tested",
+            "Well-Worn",
+            "Battle-Scarred"});
+            this.Quality.Location = new System.Drawing.Point(230, 264);
+            this.Quality.Name = "Quality";
+            this.Quality.Size = new System.Drawing.Size(100, 24);
+            this.Quality.TabIndex = 5;
             // 
-            // listBox1
+            // label2
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(591, 356);
-            this.listBox1.TabIndex = 2;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите название скина";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 350);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите название оружия";
+            // 
+            // SkinName
+            // 
+            this.SkinName.Location = new System.Drawing.Point(230, 311);
+            this.SkinName.Name = "SkinName";
+            this.SkinName.Size = new System.Drawing.Size(100, 22);
+            this.SkinName.TabIndex = 8;
+            // 
+            // WeaponName
+            // 
+            this.WeaponName.Location = new System.Drawing.Point(230, 347);
+            this.WeaponName.Name = "WeaponName";
+            this.WeaponName.Size = new System.Drawing.Size(100, 22);
+            this.WeaponName.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(404, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(215, 47);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Выполнить сравнение";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(713, 225);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(274, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ссылка на лучший вариант";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(713, 411);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.WeaponName);
+            this.Controls.Add(this.SkinName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Quality);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SkinsParser";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Quality;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SkinName;
+        private System.Windows.Forms.TextBox WeaponName;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- SkinsParser/Form1.cs
+++ SkinsParser/Form1.cs
@@ -26,38 +26,88 @@ using Json.Net;
 using System.Numerics;
 using SkinsParser.Core.csmoney;
 using System.Reactive;
+using SkinsParser.Core.steam_community_market;
 
 namespace SkinsParser
 {
+    delegate DialogResult ErrorMessage(string a, string b, MessageBoxButtons c, MessageBoxIcon d);
     public partial class Form1 : Form
     {
+        ErrorMessage errorMessage = MessageBox.Show;
         CsgotmWorker csgotm;
         CsMoneyWorker csMoney;
+        SteamCommunityMarketWorker steamCommunityMarketWorker;
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            textBox1.Text = "None";
+            Quality.Text = "Field-Tested";
+            WeaponName.Text = "AWP";
+            SkinName.Text = "Neo-Noir";
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].HeaderCell.Value = "site";
+            dataGridView1.Columns[1].HeaderCell.Value = "price";
+            dataGridView1.Columns[2].HeaderCell.Value = "URL";
+            dataGridView1.Rows[0].Cells[0].Value = "csgotm";
+            dataGridView1.Rows[1].Cells[0].Value = "csMoney";
+            dataGridView1.Rows[2].Cells[0].Value = "steamMarket";
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Height = dataGridView1.Rows.GetRowsHeight(DataGridViewElementStates.Visible) + 
+                dataGridView1.ColumnHeadersHeight;
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private async void button4_Click(object sender, EventArgs e)
         {
-            csgotm = new CsgotmWorker("", "");
-            listBox1.Items.Add(csgotm.GetItem("AWP | Worm God (Factory New)"));
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private async void button2_Click(object sender, EventArgs e)
-        {
-            csMoney = new CsMoneyWorker("", "");
-            listBox1.Items.Add(await csMoney.GetItem("M4A4%20Neo-Noir"));
+            var minPriceUrl = new Tuple<string, double>("None", Double.MaxValue);
+            var quality = Quality.Text;
+            var weapon = WeaponName.Text;
+            var skin = SkinName.Text;
+            csgotm = new CsgotmWorker(weapon, quality, skin);
+            csMoney = new CsMoneyWorker(weapon, quality, skin);
+            steamCommunityMarketWorker = new SteamCommunityMarketWorker(weapon, quality, skin);
+            dataGridView1.Rows[0].Cells[2].Value = csgotm.GetURL();
+            dataGridView1.Rows[1].Cells[2].Value = csMoney.GetURL();
+            dataGridView1.Rows[2].Cells[2].Value = steamCommunityMarketWorker.GetURL();
+            try
+            {
+                dataGridView1.Rows[0].Cells[1].Value = (await csgotm.GetItem());
+                Double.TryParse((string)dataGridView1.Rows[0].Cells[1].Value, out double result);
+                if (minPriceUrl.Item2 > result)
+                    minPriceUrl = new Tuple<string, double>(csgotm.GetURL(), result);
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Rows[0].Cells[1].Value = "None";
+                errorMessage(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            try
+            {
+                dataGridView1.Rows[1].Cells[1].Value = (await csMoney.GetItem());
+                Double.TryParse((string)dataGridView1.Rows[1].Cells[1].Value, out double result);
+                if (minPriceUrl.Item2 > result)
+                    minPriceUrl = new Tuple<string, double>(csMoney.GetURL(), result);
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Rows[1].Cells[1].Value = "None";
+                errorMessage(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            try
+            {
+                dataGridView1.Rows[2].Cells[1].Value = (await steamCommunityMarketWorker.GetItem());
+                Double.TryParse((string)dataGridView1.Rows[2].Cells[1].Value, out double result);
+                if (minPriceUrl.Item2 > result)
+                    minPriceUrl = new Tuple<string, double>(steamCommunityMarketWorker.GetURL(), result);
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Rows[2].Cells[1].Value = "None";
+                errorMessage(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            textBox1.Text = minPriceUrl.Item1;
         }
     }
 }
